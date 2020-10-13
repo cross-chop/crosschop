c219403c051dd3bf796a36163e08d0a8b927f51b
Satoshi Tamura <bofft@mac.com>
2020-10-13 22:24:23 +0900

    text hide

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,9 @@
   <div class="container">
     <div>
       <h1 class="title">
-        <img src="~/assets/img/logo.svg" alt="cross chop" />
+        <img src="~/assets/img/logo.svg" alt="cross chop クロスチョップ" />
       </h1>
       <p class="subtitle">We know HTML, CSS and Javascript.</p>
-      <p class="note">build test</p>
     </div>
   </div>
 </template>
@@ -21,7 +20,7 @@ export default {}
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  // text-align: center;
 }
 .title {
   img {
@@ -38,9 +37,35 @@ export default {}
   letter-spacing: 0.03em;
   color: #000;
   margin: 0 0 0 -5px;
+  text-align: center;
   // word-spacing: 5px;
   // padding-bottom: 15px;
 }
+h2 {
+  font-size: 18px;
+  text-align: center;
+  margin-top: 20px;
+}
+ul {
+  display: flex;
+  // background: red;
+  padding: 0;
+  margin: 0;
+  justify-content: center;
+  li {
+    // background: blue;
+    list-style: none;
+    margin: 0 10px;
+    width: 50%;
+    &:first-child {
+      text-align: right;
+    }
+    &:last-child {
+      text-align: left;
+    }
+  }
+}
+
 .note {
   margin-top: 1em;
 }
